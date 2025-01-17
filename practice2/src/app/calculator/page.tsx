4caf83d248efa9a6a37e699cbb4ff52a64ec25d9
hussainezzi <hussainezzi03@gmail.com>
2025-01-17 16:38:16 +0500

    stared to rmake a calculato

---
+++ practice2/src/app/calculator/page.tsx
@@ -0,0 +1,9 @@
+// i write the code ot make a calculator plz assis me to make it 
+import React , {useState} from 'react';
+// why are we importing app.csss??
+import './App.css';
+
+
+export default function Calculator(){
+    
+}
