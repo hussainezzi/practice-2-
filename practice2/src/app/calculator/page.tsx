1ccd55e978faec71d49986b5a2461de4f650e165
hussainezzi <hussainezzi03@gmail.com>
2025-01-17 17:06:28 +0500

    addded the buttonss

--- practice2/src/app/calculator/page.tsx
+++ practice2/src/app/calculator/page.tsx
@@ -6,6 +6,8 @@ import './App.css';
 
 export default function Calculator(){
     const [result , setResult ] = useState("");
+  
+  
     const handleClick  = (e) =>{
         setResult(result.concat(e.target.name))
     }
@@ -14,3 +16,44 @@ export default function Calculator(){
 const clear = () =>{
     setResult("");
 }
+
+const backspace = () =>{
+    setResult(result.slice(0, -1));
+}
+
+const calculate try =(){
+    setResult()
+}
+
+    return(
+        <div className = "container">
+            <div className =" flex flex-row gap-3">
+                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>1</div>
+                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>2</div>
+                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>3</div>
+            </div>
+
+
+            <div className = "container">
+            <div className =" flex flex-row gap-3">
+                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>1</div>
+                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>2</div>
+                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>3</div>
+            </div>
+
+            <div className = "container">
+            <div className =" flex flex-row gap-3">
+                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>1</div>
+                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>2</div>
+                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>3</div>
+            </div>
+
+            <div className = "container">
+            <div className =" flex flex-row gap-3">
+                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>1</div>
+                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>2</div>
+                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>3</div>
+            </div>
+
+        </div>
+    )
