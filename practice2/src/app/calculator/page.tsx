0392263eec047eab42139f3639a0778dcbcbddf7
hussainezzi <hussainezzi03@gmail.com>
2025-01-17 17:13:53 +0500

    completed the calculator

--- practice2/src/app/calculator/page.tsx
+++ practice2/src/app/calculator/page.tsx
@@ -1,59 +1,55 @@
-// i write the code ot make a calculator plz assis me to make it 
-import React , {useState} from 'react';
-// why are we importing app.csss??
+import React, { useState } from 'react';
 import './App.css';
 
+export default function Calculator() {
+  const [result, setResult] = useState("");
 
-export default function Calculator(){
-    const [result , setResult ] = useState("");
-  
-  
-    const handleClick  = (e) =>{
-        setResult(result.concat(e.target.name))
-    }
-}
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name));
+  };
 
-const clear = () =>{
+  const clear = () => {
     setResult("");
-}
+  };
 
-const backspace = () =>{
+  const backspace = () => {
     setResult(result.slice(0, -1));
+  };
+
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (error) {
+      setResult("Error");
+    }
+  };
+
+  return (
+    <div className="container">
+      <form>
+        <input type="text" value={result} readOnly />
+      </form>
+
+      <div className="keypad">
+        <button onClick={clear} id="clear">Clear</button>
+        <button onClick={backspace} id="backspace">C</button>
+        <button name="/" onClick={handleClick}>&divide;</button>
+        <button name="7" onClick={handleClick}>7</button>
+        <button name="8" onClick={handleClick}>8</button>
+        <button name="9" onClick={handleClick}>9</button>
+        <button name="*" onClick={handleClick}>&times;</button>
+        <button name="4" onClick={handleClick}>4</button>
+        <button name="5" onClick={handleClick}>5</button>
+        <button name="6" onClick={handleClick}>6</button>
+        <button name="-" onClick={handleClick}>&ndash;</button>
+        <button name="1" onClick={handleClick}>1</button>
+        <button name="2" onClick={handleClick}>2</button>
+        <button name="3" onClick={handleClick}>3</button>
+        <button name="+" onClick={handleClick}>+</button>
+        <button name="0" onClick={handleClick}>0</button>
+        <button name="." onClick={handleClick}>.</button>
+        <button onClick={calculate} id="result">=</button>
+      </div>
+    </div>
+  );
 }
-
-const calculate try =(){
-    setResult()
-}
-
-    return(
-        <div className = "container">
-            <div className =" flex flex-row gap-3">
-                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>1</div>
-                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>2</div>
-                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>3</div>
-            </div>
-
-
-            <div className = "container">
-            <div className =" flex flex-row gap-3">
-                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>1</div>
-                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>2</div>
-                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>3</div>
-            </div>
-
-            <div className = "container">
-            <div className =" flex flex-row gap-3">
-                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>1</div>
-                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>2</div>
-                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>3</div>
-            </div>
-
-            <div className = "container">
-            <div className =" flex flex-row gap-3">
-                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>1</div>
-                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>2</div>
-                <div className = "p-3 text-white bg-grey-800 rounded-md border-black-800" onClick  = {handleClick}>3</div>
-            </div>
-
-        </div>
-    )
